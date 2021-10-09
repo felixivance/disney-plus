d047754132dd38652a72429438e03327cc3f037c
Felix <felixrunye@gmail.com>
2021-10-09 13:45:18 +0300

    navbar update

--- components/Header.js
+++ components/Header.js
@@ -6,11 +6,11 @@ function Header() {
         // container
         <div className="h-16 bg-[#090b13] flex items-center justify-between  pl-4 pr-4">
             {/* logo */}
-            <div className="w-20">
+            <div className="w-20 ">
                 <Image src={Logo} layout="responsive" />
             </div>
             {/* menu */}
-            <div className="flex items-center flex-grow ml-5 ">
+            <div className="flex items-center ml-5 ">
                 <a className="flex items-center pl-3 pr-3 cursor-pointer ">
                     <img src="/images/home-icon.svg" className="h-8 md:h-5" alt="" />
                     <div className=" hover:border-b-2 hover:border-white">
@@ -18,7 +18,7 @@ function Header() {
                     </div>
                 </a>
                 <a className="flex items-center pl-3 pr-3 cursor-pointer">
-                    <img src="/images/search-icon.svg" className="h-8 md:h-5" alt="" />
+                    <img src="/images/search-icon.svg" className="h-8 md:h-5 " alt="" />
                     <div className=" hover:border-b-2 hover:border-white">
                         <p className="text-white text-[13px] tracking-[1.42px] hidden md:inline-flex">SEARCH</p> 
                     </div>
@@ -50,7 +50,7 @@ function Header() {
                 </a>
             </div>
             {/* user image div */}
-            <div className="">
+            <div className="flex">
                 <img src="https://lh3.googleusercontent.com/a-/AOh14GjkbYYp7lahAZhwF2jCOwBnPjXXYZhGNfuvvi9O8dQ=s96-c" 
                 className="w-12 h-12 rounded-full"/>
             </div>
