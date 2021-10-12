5a0845a4ebc25632269b6d873abcaa3804970ef9
Felix <felixrunye@gmail.com>
2021-10-12 09:14:39 +0300

    updated cards

--- components/Movies.js
+++ components/Movies.js
@@ -1,10 +1,10 @@
 function Movies() {
     return (
         // container
-        <div>
+        <div className="space-y-2 pt-2">
             <h4>Recommended for you</h4>
             {/* content */}
-            <div className="grid grid-cols-5 gap-3">
+            <div className="grid grid-cols-4 gap-3 pb-2 pl-1 pr-2">
                 <div className="">
                     <img src="https://whatsondisneyplus.com/wp-content/uploads/2019/10/EG1tpkVVUAEU30q.jpg" className="rounded-sm" />
                 </div>
@@ -12,14 +12,12 @@ function Movies() {
                     <img src="https://whatsondisneyplus.com/wp-content/uploads/2019/10/EG1tpkVVUAEU30q.jpg" className="rounded-sm" />
                 </div>
                 <div>
-                    <img src="https://whatsondisneyplus.com/wp-content/uploads/2019/10/EG1tpkVVUAEU30q.jpg" className="rounded-sm" />
+                    <img src="https://whatsondisneyplus.com/wp-content/uploads/2019/10/EG1tpkVVUAEU30q.jpg" className="rounded-sm " />
                 </div>
                 <div>
                     <img src="https://whatsondisneyplus.com/wp-content/uploads/2019/10/EG1tpkVVUAEU30q.jpg"  className="rounded-sm"/>
                 </div>
-                <div>
-                    <img src="https://whatsondisneyplus.com/wp-content/uploads/2019/10/EG1tpkVVUAEU30q.jpg" className="rounded-sm"/>
-                </div>
+               
             </div>
         </div>
     )
