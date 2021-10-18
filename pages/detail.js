77b5e95a3c45e877747f2640237f5b6b57b63754
Felix <felixrunye@gmail.com>
2021-10-18 21:09:04 +0300

    styling of detail page

--- pages/detail.js
+++ pages/detail.js
@@ -1,8 +1,16 @@
+import Head from 'next/head';
+import Header
+ from '../components/Header';
 function Detail() {
     return (
         <div>
-            
-            
+             <Head>
+                 <title>Disney +</title>
+            </Head>
+            <Header/>
+            <div className="homeContainer bg-home-img bg-center bg-cover bg-no-repeat bg-fixed inset-0 -z-1 ">
+
+            </div>
         </div>
     )
 }
