336eee85a9bc82663fff1942b520ce337a466768
Felix <felixrunye@gmail.com>
2021-10-18 21:02:27 +0300

    styling of movie list

--- components/Movies.js
+++ components/Movies.js
@@ -5,16 +5,16 @@ function Movies() {
             <h4>Recommended for you</h4>
             {/* content */}
             <div className="grid grid-cols-4 gap-3 pb-2 pl-1 pr-2">
-                <div className="shadow-md">
+                <div className="shadow-md hover:scale-105 transition-all duration-150 cursor-pointer">
                     <img  src="https://whatsondisneyplus.com/wp-content/uploads/2019/10/EG1tpkVVUAEU30q.jpg" className="rounded-sm" />
                 </div>
-                <div>
+                <div className="hover:scale-105 transition-all duration-150 cursor-pointer">
                     <img src="https://whatsondisneyplus.com/wp-content/uploads/2019/10/EG1tpkVVUAEU30q.jpg" className="rounded-sm" />
                 </div>
-                <div>
+                <div className="hover:scale-105 transition-all duration-150 cursor-pointer">
                     <img src="https://whatsondisneyplus.com/wp-content/uploads/2019/10/EG1tpkVVUAEU30q.jpg" className="rounded-sm " />
                 </div>
-                <div>
+                <div className="hover:scale-105 transition-all duration-150 cursor-pointer">
                     <img src="https://whatsondisneyplus.com/wp-content/uploads/2019/10/EG1tpkVVUAEU30q.jpg"  className="rounded-sm"/>
                 </div>
                
