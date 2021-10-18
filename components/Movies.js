98b42ff27d2ddceb1a23b8c001effb592758d214
Felix <felixrunye@gmail.com>
2021-10-18 21:00:23 +0300

    movies list

--- components/Movies.js
+++ components/Movies.js
@@ -5,8 +5,8 @@ function Movies() {
             <h4>Recommended for you</h4>
             {/* content */}
             <div className="grid grid-cols-4 gap-3 pb-2 pl-1 pr-2">
-                <div className="">
-                    <img src="https://whatsondisneyplus.com/wp-content/uploads/2019/10/EG1tpkVVUAEU30q.jpg" className="rounded-sm" />
+                <div className="shadow-md">
+                    <img  src="https://whatsondisneyplus.com/wp-content/uploads/2019/10/EG1tpkVVUAEU30q.jpg" className="rounded-sm" />
                 </div>
                 <div>
                     <img src="https://whatsondisneyplus.com/wp-content/uploads/2019/10/EG1tpkVVUAEU30q.jpg" className="rounded-sm" />
