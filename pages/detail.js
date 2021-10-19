0c737f3767f2ac22314644e45e3a8b88e291aff7
Felix <felixrunye@gmail.com>
2021-10-19 08:48:03 +0300

    updated detail page

--- pages/detail.js
+++ pages/detail.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
-import Header
- from '../components/Header';
+import Header from '../components/Header';
+
 function Detail() {
     return (
         <div>
@@ -9,7 +9,10 @@ function Detail() {
             </Head>
             <Header/>
             <div className="homeContainer bg-home-img bg-center bg-cover bg-no-repeat bg-fixed inset-0 -z-1 ">
-
+                {/* bg component */}
+                <div className="relative h-full ">
+                    <img src="https://cdn.wallpapersafari.com/63/91/EGjUcK.jpg" className="h-screen object-cover md:object-fill fixed top-16  left-0 bottom-0 right-0 opacity-[0.8] " alt="" />
+                </div>
             </div>
         </div>
     )
